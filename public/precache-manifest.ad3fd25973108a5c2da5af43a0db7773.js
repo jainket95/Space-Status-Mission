0d3c98823f0cb12b92d1a2e318bcaa9a43b0ecbf
thelucidrealist <aniket1995jain@gmail.com>
2019-07-23 23:30:40 +0530

    Final Heroku Commit

---
+++ public/precache-manifest.ad3fd25973108a5c2da5af43a0db7773.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d152d3b920ba71c8d777c1154595b61f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "841909d9d8623def6a5b",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "1e9b176282eba694421a",
+    "url": "/static/js/2.9a1e68e1.chunk.js"
+  },
+  {
+    "revision": "841909d9d8623def6a5b",
+    "url": "/static/js/main.2b58259a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  }
+]);
