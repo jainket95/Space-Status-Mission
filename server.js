cce0f6f58b089af4fc291042b98fd37c0edf6f71
thelucidrealist <aniket1995jain@gmail.com>
2019-07-23 21:35:25 +0530

    Finished

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 
-app.use(cors()); 
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -16,6 +16,9 @@ app.use(
 );
 
 app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
